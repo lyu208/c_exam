5359a0c5358966680a9b77b4fde5a6a1c0c12659
chain01 <15863338830@163.com>
2018-11-13 20:03:10 +0800

    1day

---
+++ 1day/1day.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int g = 0;
+int f()
+{
+    return g++;
+}
+int main()
+{
+    if( f() && f() )
+	{
+		printf("%d\n", g);
+    }    
+    printf("%d\n", g);    
+    return 0;
+}
